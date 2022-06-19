e2a1f6a457ba72bec7ed87af3eb897e65c4dc9d2
Defernus <dehucka98@gmail.com>
2022-06-19 12:22:11 +0300

    improve flight physics

--- src/plugins/player/systems/move_system/move_glide.rs
+++ src/plugins/player/systems/move_system/move_glide.rs
@@ -3,6 +3,10 @@ use bevy::prelude::*;
 use crate::plugins::player::components::PlayerComponent;
 
 const G_FORCES: f32 = 10.;
+
+const SIDE_DRAG: f32 = 7.;
+const SHIFT_SIZE_DRAG: f32 = 5.;
+
 const DRAG: f32 = 0.5;
 const SHIFT_DRAG: f32 = 0.01;
 
@@ -12,17 +16,28 @@ pub fn move_glide(
     transform: &mut Transform,
     dt: f32,
 ) {
-    let drag = if keys.pressed(KeyCode::LShift) {
+    let drag_val = if keys.pressed(KeyCode::LShift) {
         SHIFT_DRAG
     } else {
         DRAG
     };
+    let side_drag_val = if keys.pressed(KeyCode::LShift) {
+        SHIFT_SIZE_DRAG
+    } else {
+        SIDE_DRAG
+    };
 
     let forward = transform.forward();
-    let speed = player.speed.dot(forward).max(0.)
-        - forward.y * G_FORCES * dt
-        - player.speed.length() * drag * dt;
-    player.speed = transform.forward() * speed;
+
+    let forward_speed = player.speed.dot(forward) * forward;
+
+    let drag_a = -player.speed * drag_val;
+    let g_a = -Vec3::Y * G_FORCES;
+    let side_drag_a = (forward_speed - player.speed) * side_drag_val;
+
+    let a = drag_a + g_a + side_drag_a;
+
+    player.speed += a * dt;
 
     transform.translation += player.speed * dt;
 }
